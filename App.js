377ebcfb284b35bd985a22e96667828cfb2c7c8b
Asimamir25 <asimamir32@gmail.com>
2024-05-22 00:51:18 +0500

    first commit

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View} from 'react-native';
+import React, {useEffect} from 'react';
+import firestore from '@react-native-firebase/firestore';
 
 const App = () => {
+  useEffect(() => {
+    firestore()
+      .collection('Users')
+      .add({
+        name: 'asim',
+        age: 30,
+      })
+      .then(() => {
+        console.log('User added!');
+      }, []);
+  });
   return (
     <View>
       <Text>App</Text>
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
